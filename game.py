03bfb8371552dc6ad79c5065265e770936051345
Lola Danhaive <angelicalola.danhaive@gmail.com>
2023-01-11 10:14:29 +0000

    Fixed errors and added tests
    1. Simplified the search for the next guess by only having one probability_distribution variable which is assigned at the very beggining of the game (so that I can easily change it if I want to test other methods). All guesses are chosen based on this, which simplifies various functions in game.py. 2.I generated a solution before starting the game so that the code can easily be run on the whole list of words for example. Before, in the bash mode, it only accepted an input from a user for a solution.
    3. I had a problem with values of zero in the score array (which were assigned when the word had 2 or more of the same letters). This didn't work because it could cause Nan to appear when the scores array was renormalized (because of division by zero. This is now fixed because 0.0 is never assigned, and a test to make sure the array is all non-zero has been added.
    4. The test mentioned above is included in the new test_game.py module which for now only tests initialize to make sure all of the outputs are as expected.
    5. Some documentation has been improved

--- game.py
+++ game.py
@@ -12,6 +12,10 @@ import words
 import probabilities as proba
 import numpy as np
 
+#to print the full lengh of arrays
+import sys
+np.set_printoptions(threshold=sys.maxsize)
+
 # @profile
 def initialize():
 	'''
@@ -23,133 +27,135 @@ def initialize():
 		Returns
 		----------
 		interactive
-			bool corresponding to the player' s choice to play the interactive version or not
+			bool corresponding to the player's choice to play the interactive version or not
 		words_list
 			the list of all words accepted by Wordle
 		scores
-			scores given to each word
+			scores given to each word based on letter frequencies (in words and in a particular place in the word)
 		frequencies
 			frequencies of each word in the english language
 	'''
 	answer = input('Welcome to the Wordle Solver, would you like to play interactively or ask me to solve for a word? Write INTERACT or SOLVE and press ENTER: ')
-	correct = False
-	while not correct:
+	#make sure only accepted answers are given, run the loop until the answer is valid
+	is_correct = False
+	while not is_correct:
 		if answer.lower() == 'interact':
 			interactive = True
-			correct = True
+			is_correct = True
 		elif answer.lower() == 'solve':
 			interactive = False
-			correct = True
+			is_correct = True
 		else:
 			answer = input('Invalid answer, please try again: ')
 
 
 	#load of all the words into a list
-	with open('WordleWords.txt') as file:
-		words_list = [line.rstrip() for line in file] 
+	words_list = words.load_list()
 
-	#compute the probabilities of the letters and give the words a score => highest score will be the first guess 
-	#(this could also be done separately and the results saved in a txt file.. see how long it takes and then decide)
+	#compute the arrays containing the scores and frequencies of each word in the list
 	scores, frequencies = proba.compute_all(words_list)
 
 	#normalize the frequencies and scores so that they sum to 1 and hence can be used as probabilities
-	frequencies = frequencies/frequencies.sum()
-	scores = scores/scores.sum()
+	frequencies = proba.renormalize(frequencies)
+	scores = proba.renormalize(scores)
 
 
 	return interactive, words_list, scores, frequencies
 # @profile
-def guess_run(words_list, scores, frequencies,interactive):
+
+def pick_solution(words_list,interactive):
+	'''
+		Function that picks the solution word based on which version the user wants to play 
+
+		Parameters
+		----------
+		words_list
+			array, the list of all words accepted by Wordle
+		interactive
+			boolean, True if interactive, false if solve
+
+		Returns
+		----------
+		solution
+			string, the word chosen as the solution to the round of Wordle
+	'''
+	#word chosen randomly from the words_list or chosen by the user
+	if interactive:
+		solution = np.random.choice(words_list,size = None, replace = True)
+	else:
+		solution = input('Type in the 5 letter word you would like me to guess (I wont peek!!), then press ENTER: ')
+		solution = (words.verify(solution)).lower() #always lower strings to make sure there is no capitalization error
+	return solution
+
+def guess_run(words_list, solution, probability_distribution,interactive):
 	'''
 		Function that makes the computer guess until the solution given by the user is found
 
 		Parameters
 		----------
 		words_list
-			the list of all words accepted by Wordle
-		scores
-			scores given to each word
-		frequencies
-			frequencies of each word in the english language
+			array, the list of all words accepted by Wordle
+		probability_distribution
+			array of the chosen probability distribution for the list of possible words
 		interactive
-			True if interactive, false if solve
+			boolean, True if interactive, false if solve
 		Returns
 		----------
 		tries
-			the number of guesses until finding the right one
+			int, the number of guesses until finding the right one
 	'''
 	tries = 1
 	solution_found = False
-	#1. the user is asked to give a five letter word
-	if interactive:
-		solution = np.random.choice(words_list,size = None, replace = True, p= frequencies)
-	else:
-		solution = input('Type in the 5 letter word you would like me to guess (I wont peek!!), then press ENTER: ')
-		solution = (words.verify(solution)).lower() #always do the word check with the full list
 
-	guess1, words_list_1,frequencies_1 = try_guess(solution, words_list, frequencies, interactive, scores, 1) 
+	guess1, words_list_1,probability_distribution1 = try_guess(solution, words_list, probability_distribution,interactive) 
 	
 	tries +=1
-	guess2, words_list_2,frequencies_2 = try_guess(solution,words_list,frequencies, interactive, number = 2, previous_guess = guess1)
+	guess2, words_list_2,probability_distribution2 = try_guess(solution,words_list,probability_distribution, interactive, previous_guess = guess1)
 	#the words_list used is the intersection of the two resulting from the first two guesses
 	words_list,indices1,indices2 = np.intersect1d(words_list_1,words_list_2,assume_unique=False, return_indices=True)
-	frequencies = np.take(frequencies_1, indices1)
+	probability_distribution = np.take(probability_distribution1, indices1)
 	#renormalize
-	frequencies = frequencies/frequencies.sum()
+	probability_distribution = proba.renormalize(probability_distribution)
 
 	#loop until the right word is found
 	while not solution_found:
 		tries+= 1
-		guess, words_list, frequencies  = try_guess(solution, words_list, frequencies, interactive)
+		guess, words_list, probability_distribution  = try_guess(solution, words_list, probability_distribution, interactive)
 
 		if guess == solution:
 			solution_found = True
 
 	return tries, solution
 # @profile
-def try_guess(solution, words_list, frequencies, interactive, scores = None, number = 0, previous_guess = None):
+def try_guess(solution, words_list, probability_distribution, interactive, previous_guess = None):
 	'''
 		Function to make the user/computer guess
 
 		Parameters
 		----------
 		solution
-			of the wordle run
+			string, solution of the wordle run
 		words_list
-			the list of all words accepted by Wordle
-		frequencies
-			frequencies of each word in the english language
+			array, the list of all words accepted by Wordle
+		probability_distribution
+			array of the chosen probability distribution for the list of possible words
 		interactive
-			True if interactive, false if solve
-		scores
-			scores given to each word (needed if we are at guess 1)
-		number
-			1 for first guess, 2 for second guess, 0 for all of the others. This determines the method to chose the next suggestion
+			boolean, True if interactive, false if solve
 		previous_guess
-			previous guess (needed if we are at guess 2)
+			string, previous guess (needed if we are at guess 2)
 
 		Returns
 		----------
 		guess
-			the guess
+			string, the guess
 		words_list
-			the list of all words that can still be the answer
-		frequencies
-			their frequencies
+			array, the list of all words that can still be the answer
+		probability_distribution
+			array of the chosen probability distribution for the list of possible words
 	'''
+
 	#generate the suggestion depending on the number of the guess
-	if number == 1:
-		suggestion = np.random.choice(words_list,size = None, replace = True, p= scores)	
-	elif number == 2:
-		difference_score = proba.compute_difference_score(words_list,previous_guess) 
-		#only select from words that have all different letters than guess 1
-		indices = [i for i, x in enumerate(difference_score) if x == 5] 
-		new_frequencies = np.take(frequencies,indices)
-		new_words_list = np.take(words_list,indices) #containing only the words with a score of 5
-		new_frequencies = new_frequencies/sum(new_frequencies)
-		suggestion = np.random.choice(new_words_list,size = None, replace = True,p=new_frequencies)
-	else:
-		suggestion = np.random.choice(words_list,size = None, replace = True, p= frequencies)
+	suggestion = generate_guess(words_list, probability_distribution, interactive, previous_guess)
 	#obtain/generate the guess depending on the chosen mode 
 	if interactive:
 		#give the option to see the full list of possible words at this stage
@@ -164,14 +170,50 @@ def try_guess(solution, words_list, frequencies, interactive, scores = None, num
 		guess = suggestion
 		print('I am guessing the word {}. '.format(suggestion))
 
-	#3. output the response array of colors
+	#exit the function if the guess matches the solution
+	if guess.lower() == solution:
+		return guess, None, None
+	
+	#compute the response array of colors
 	response = words.compare(guess, solution)
 	print('Here are the results: \n', response[0], response[1],response[2],response[3], response[4])
-	# words_list is modified to only include possible words
-	words_list,frequencies = words.sort(words_list, frequencies, guess, response)	
-	frequencies = np.array(frequencies)/sum(frequencies)
 
-	return guess, words_list,frequencies	
+	# words_list is modified to only include possible words, and the frequency array is modified accordingly
+	words_list,probability_distribution = words.sort(words_list, probability_distribution, guess, response)
 
+	probability_distribution = proba.renormalize(probability_distribution)
 
+	return guess, words_list, probability_distribution	
+
+def generate_guess(words_list, probability_distribution, interactive, previous_guess = None):
+	'''
+		Function to generate the guess/suggestion
+
+		Parameters
+		----------
+		words_list
+			the list of all words accepted by Wordle
+		probability_distribution
+			array of the chosen probability distribution for the list of possible words
+		interactive
+			True if interactive, false if solve
+		previous_guess
+			previous guess (needed for second guess) or None if any other guess
+
+		Returns
+		----------
+		suggestion
+			string, for the next guess
+	'''
+	if previous_guess == None:
+		suggestion = np.random.choice(words_list,size = None, replace = True, p= probability_distribution)
+	else:
+		difference_score = proba.compute_difference_score(words_list,previous_guess) 
+		#only select from words that have all different letters than guess 1
+		indices = [i for i, x in enumerate(difference_score) if x == 5] 
+		new_frequencies = np.take(probability_distribution,indices)
+		new_words_list = np.take(words_list,indices) #containing only the words with a score of 5
+		new_frequencies = new_frequencies/sum(new_frequencies)
+		suggestion = np.random.choice(new_words_list,size = None, replace = True,p=new_frequencies)		
 		
+	return suggestion
